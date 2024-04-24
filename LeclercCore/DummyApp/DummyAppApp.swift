c37001c2e193d9a788defe25419a84f7c6018e5e
Sofía Jiménez Martínez <sofyjmtz@hotmail.com>
2024-04-24 05:27:24 -0600

    [UPDATE] ALL TESTS PASSED

---
+++ LeclercCore/DummyApp/DummyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DummyAppApp.swift
+//  DummyApp
+//
+//  Created by Sofía Jimémez Martínez on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DummyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- LeclercCore/LeclercCore/FirestoreManager.swift
+++ LeclercCore/LeclercCore/FirestoreManager.swift
@@ -39,8 +39,18 @@ class FirestoreManager {
     }
 
     func addMedalToUser(userId: String, medal: Activity, completion: @escaping (Result<Void, Error>) -> Void) {
+        
+        let medalData = [
+            "id": medal.id,
+            "name": medal.name,
+            "description": medal.description,
+            "type": medal.type,
+            "date": medal.date
+        ] as [String : Any]
+        
+        
         db.collection("users").document(userId).updateData([
-            "medals": FieldValue.arrayUnion([medal])
+            "medals": FieldValue.arrayUnion([medalData])
         ]) { error in
             if let error = error {
                 completion(.failure(error))
@@ -50,9 +60,40 @@ class FirestoreManager {
         }
     }
 
+
     func addQuestToUser(userId: String, quest: Activity, completion: @escaping (Result<Void, Error>) -> Void) {
+
+        let questData = [
+            "id": quest.id,
+            "name": quest.name,
+            "description": quest.description,
+            "type": quest.type,
+            "date": quest.date
+        ] as [String : Any]
+
         db.collection("users").document(userId).updateData([
-            "quests": FieldValue.arrayUnion([quest])
+            "quests": FieldValue.arrayUnion([questData])
+        ]) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+
+    // addMyFeedToUser
+    func addMyFeedToUser(userId: String, feed: Feed, completion: @escaping (Result<Void, Error>) -> Void) {
+        let feed = [
+            "id": feed.id,
+            "lat": feed.lat,
+            "lng": feed.lng,
+            "name": feed.name,
+            "reactions": feed.reactions
+        ] as [String : Any]
+
+        db.collection("users").document(userId).updateData([
+            "feeds.my_feed": feed
         ]) { error in
             if let error = error {
                 completion(.failure(error))
@@ -63,8 +104,16 @@ class FirestoreManager {
     }
 
     func addEventToUser(userId: String, event: Activity, completion: @escaping (Result<Void, Error>) -> Void) {
+        let eventData = [
+            "id": event.id,
+            "name": event.name,
+            "description": event.description,
+            "type": event.type,
+            "date": event.date
+        ] as [String : Any]
+
         db.collection("users").document(userId).updateData([
-            "eventos": FieldValue.arrayUnion([event])
+            "eventos": FieldValue.arrayUnion([eventData])
         ]) { error in
             if let error = error {
                 completion(.failure(error))
@@ -75,6 +124,14 @@ class FirestoreManager {
     }
 
     func addVisitedFeedToUser(userId: String, feed: Feed, completion: @escaping (Result<Void, Error>) -> Void) {
+        let feed = [
+            "id": feed.id,
+            "lat": feed.lat,
+            "lng": feed.lng,
+            "name": feed.name,
+            "reactions": feed.reactions
+        ] as [String : Any]
+
         db.collection("users").document(userId).updateData([
             "feeds.feeds_visites": FieldValue.arrayUnion([feed])
         ]) { error in
@@ -85,6 +142,9 @@ class FirestoreManager {
             }
         }
     }
+
+    // function to add my_feed to user
+    
 
     // Function to read a user
     func getUser(userID: String, completion: @escaping (User?) -> Void) {
@@ -159,6 +219,82 @@ class FirestoreManager {
         }
     }
 
+    // function get all medals of a user
+    func getMedalsOfUser(userID: String, completion: @escaping (Result<[Activity], Error>) -> Void) {
+        db.collection("users").document(userID).getDocument { (document, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let document = document, document.exists {
+                let data = document.data()
+                let medals = (data?["medals"] as? [[String: Any]]) ?? []
+                let userMedals = medals.map { medalData in
+                    Activity(id: medalData["id"] as? String ?? "",
+                             name: medalData["name"] as? String ?? "",
+                             description: medalData["description"] as? String ?? "",
+                             type: medalData["type"] as? String ?? "",
+                             date: Date())
+                }
+                completion(.success(userMedals))
+            } else {
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Document does not exist"])
+                completion(.failure(error))
+            }
+        }
+    }
+
+    // function get all quests of a user
+    func getQuestsOfUser(userID: String, completion: @escaping (Result<[Activity], Error>) -> Void) {
+        db.collection("users").document(userID).getDocument { (document, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let document = document, document.exists {
+                let data = document.data()
+                let quests = (data?["quests"] as? [[String: Any]]) ?? []
+                let userQuests = quests.map { questData in
+                    Activity(id: questData["id"] as? String ?? "",
+                             name: questData["name"] as? String ?? "",
+                             description: questData["description"] as? String ?? "",
+                             type: questData["type"] as? String ?? "",
+                             date: Date())
+                }
+                completion(.success(userQuests))
+            } else {
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Document does not exist"])
+                completion(.failure(error))
+            }
+        }
+    }
+
+    // function get all visited feeds of a user
+    func getVisitedFeedsOfUser(userID: String, completion: @escaping (Result<[Feed], Error>) -> Void) {
+        db.collection("users").document(userID).getDocument { (document, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let document = document, document.exists {
+                let data = document.data()
+                let feedsData = data?["feeds"] as? [String: Any] ?? [:]
+                let feeds_visites = (feedsData["feeds_visites"] as? [[String: Any]]) ?? []
+                let userFeeds = feeds_visites.map { feedData in
+                    Feed(id: feedData["id"] as? String ?? "",
+                         elements: (feedData["elements"] as? [[String: Any]] ?? []).map { elementData in
+                            Element(id: elementData["id"] as? String ?? "",
+                                    description: elementData["description"] as? String ?? "",
+                                    type: elementData["type"] as? String ?? "", position: Position(x: 0, y: 0, z: 0))
+                         },
+                         lat: feedData["lat"] as? Double ?? 0.0,
+                         lng: feedData["lng"] as? Double ?? 0.0,
+                         name: feedData["name"] as? String ?? "",
+                         reactions: feedData["reactions"] as? Int ?? 0)
+                }
+                completion(.success(userFeeds))
+            } else {
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Document does not exist"])
+                completion(.failure(error))
+            }
+        }
+    }
+
+
     // Function to delete a user
     func deleteUser(userID: String, completion: @escaping (Result<Void, Error>) -> Void) {
         db.collection("users").document(userID).delete { error in
@@ -171,32 +307,26 @@ class FirestoreManager {
     }
     
     // CRUD operations for Feeds
-    func createFeed(feed: Feed, completion: @escaping (Error?) -> Void) {
+    func createFeed(feed: Feed, completion: @escaping (Result<Void, Error>) -> Void) {
         db.collection("feeds").document(feed.id).setData([
-            "elements": feed.elements.map { element in
-                [
-                    "id": element.id,
-                    "description": element.description,
-                    "type": element.type,
-                    "position": [
-                        "x": element.position.x,
-                        "y": element.position.y,
-                        "z": element.position.z
-                    ]
-                ]
-            },
             "lat": feed.lat,
             "lng": feed.lng,
             "name": feed.name,
             "reactions": feed.reactions
         ]) { error in
-            completion(error)
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
         }
     }
 
-    func getFeed(feedID: String, completion: @escaping (Feed?) -> Void) {
+    func getFeed(feedID: String, completion: @escaping (Result<Feed, Error>) -> Void) {
         db.collection("feeds").document(feedID).getDocument { (document, error) in
-            if let document = document, document.exists {
+            if let error = error {
+                completion(.failure(error))
+            } else if let document = document, document.exists {
                 let data = document.data()
                 let elements = data?["elements"] as? [[String: Any]] ?? []
                 let lat = data?["lat"] as? Double ?? 0.0
@@ -214,16 +344,16 @@ class FirestoreManager {
                                 lng: lng,
                                 name: name,
                                 reactions: reactions)
-                completion(feed)
+                completion(.success(feed))
             } else {
-                completion(nil)
-                print("Document does not exist")
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Document does not exist"])
+                completion(.failure(error))
             }
         }
     }
     
     // CRUD Element
-    func createElement(element: Element, completion: @escaping (Error?) -> Void) {
+    func createElement(element: Element, completion: @escaping (Result<Void, Error>) -> Void) {
         db.collection("elements").document(element.id).setData([
             "description": element.description,
             "type": element.type,
@@ -233,7 +363,11 @@ class FirestoreManager {
                 "z": element.position.z
             ]
         ]) { error in
-            completion(error)
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
         }
     }
     
@@ -260,10 +394,31 @@ class FirestoreManager {
         }
     }
 
+    // add element to feed
+    func addElementToFeed(feedID: String, element: Element, completion: @escaping (Result<Void, Error>) -> Void) {
+        db.collection("feeds").document(feedID).collection("elements").document(element.id).setData([
+            "description": element.description,
+            "type": element.type,
+            "position": [
+                "x": element.position.x,
+                "y": element.position.y,
+                "z": element.position.z
+            ]
+        ]) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+
     // get all elements of a feed
-    func getElementsOfFeed(feedID: String, completion: @escaping ([Element]) -> Void) {
+    func getElementsOfFeed(feedID: String, completion: @escaping (Result<[Element], Error>) -> Void) {
         db.collection("feeds").document(feedID).collection("elements").getDocuments { (querySnapshot, error) in
-            if let querySnapshot = querySnapshot {
+            if let error = error {
+                completion(.failure(error))
+            } else if let querySnapshot = querySnapshot {
                 let elements = querySnapshot.documents.map { document in
                     let data = document.data()
                     let description = data["description"] as? String ?? ""
@@ -278,30 +433,36 @@ class FirestoreManager {
                                    type: type,
                                    position: position)
                 }
-                completion(elements)
+                completion(.success(elements))
             } else {
-                completion([])
-                print("Error getting elements of feed: \(error)")
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "No documents in snapshot"])
+                completion(.failure(error))
             }
         }
     }
     
     
     // CRUD Activity
-    func createActivity(activity: Activity, completion: @escaping (Error?) -> Void) {
+    func createActivity(activity: Activity, completion: @escaping (Result<Void, Error>) -> Void) {
         db.collection("activities").document(activity.id).setData([
             "name": activity.name,
             "description": activity.description,
             "type": activity.type,
             "date": activity.date
         ]) { error in
-            completion(error)
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
         }
     }
     
-    func getActivity(activityID: String, completion: @escaping (Activity?) -> Void) {
+    func getActivity(activityID: String, completion: @escaping (Result<Activity, Error>) -> Void) {
         db.collection("activities").document(activityID).getDocument { (document, error) in
-            if let document = document, document.exists {
+            if let error = error {
+                completion(.failure(error))
+            } else if let document = document, document.exists {
                 let data = document.data()
                 let name = data?["name"] as? String ?? ""
                 let description = data?["description"] as? String ?? ""
@@ -313,18 +474,52 @@ class FirestoreManager {
                                         description: description,
                                         type: type,
                                         date: date)
-                completion(activity)
+                completion(.success(activity))
             } else {
-                completion(nil)
-                print("Document does not exist")
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Document does not exist"])
+                completion(.failure(error))
+            }
+        }
+    }
+
+    // add addActivityToFeed
+    func addActivityToFeed(feedID: String, activity: Activity, completion: @escaping (Result<Void, Error>) -> Void) {
+        db.collection("feeds").document(feedID).collection("activities").document(activity.id).setData([
+            "name": activity.name,
+            "description": activity.description,
+            "type": activity.type,
+            "date": activity.date
+        ]) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
+            }
+        }
+    }
+
+    // add activity to user
+    func addActivityToUser(userID: String, activity: Activity, completion: @escaping (Result<Void, Error>) -> Void) {
+        db.collection("users").document(userID).collection("activities").document(activity.id).setData([
+            "name": activity.name,
+            "description": activity.description,
+            "type": activity.type,
+            "date": activity.date
+        ]) { error in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(()))
             }
         }
     }
 
     // get all activities of a user
-    func getActivitiesOfUser(userID: String, completion: @escaping ([Activity]) -> Void) {
+    func getActivitiesOfUser(userID: String, completion: @escaping (Result<[Activity], Error>) -> Void) {
         db.collection("users").document(userID).collection("activities").getDocuments { (querySnapshot, error) in
-            if let querySnapshot = querySnapshot {
+            if let error = error {
+                completion(.failure(error))
+            } else if let querySnapshot = querySnapshot {
                 let activities = querySnapshot.documents.map { document in
                     let data = document.data()
                     let name = data["name"] as? String ?? ""
@@ -333,15 +528,15 @@ class FirestoreManager {
                     let date = data["date"] as? Date ?? Date()
                     
                     return Activity(id: document.documentID,
-                                   name: name,
-                                   description: description,
-                                   type: type,
-                                   date: date)
+                                    name: name,
+                                    description: description,
+                                    type: type,
+                                    date: date)
                 }
-                completion(activities)
+                completion(.success(activities))
             } else {
-                completion([])
-                print("Error getting activities of user: \(error)")
+                let error = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "No documents in snapshot"])
+                completion(.failure(error))
             }
         }
     }

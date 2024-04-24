6020a609f2349d01cbc22fdb925197d83bc19e9f
Pablo Salas <pablosalas_@outlook.com>
2024-04-23 22:08:03 -0600

    [UPLOAD] First Commmit

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Leclerc
+//
+//  Created by Pablo Salas on 23/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeclercApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeclercApp.swift
+//  Leclerc
+//
+//  Created by Pablo Salas on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct LeclercApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
